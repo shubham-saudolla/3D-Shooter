04fc1d16b29a47493c8dd274fa1a2c59828b19ef
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-07-18 20:26:44 +0530

    Set up a gun equip method

---
+++ 3D Shooter/Assets/Scripts/Gun.cs
@@ -0,0 +1,21 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+}
---
+++ 3D Shooter/Assets/Scripts/GunController.cs
@@ -0,0 +1,34 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+	public Transform weaponHold;
+	public Gun startingGun;
+	private Gun _equippedGun;
+
+	void Start()
+	{
+		if(startingGun != null)
+		{
+			EquipGun(startingGun);
+		}
+	}
+
+	public void EquipGun(Gun gunToEquip)
+	{
+		if(_equippedGun != null)
+		{
+			Destroy(this.gameObject);
+		}
+
+		_equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+		_equippedGun.transform.parent = weaponHold;
+	}
+}
