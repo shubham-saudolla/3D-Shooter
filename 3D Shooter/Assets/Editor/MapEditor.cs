5505e65e044bfa6ab67ce3019b5c8422520bfbb1
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-07-21 01:07:23 +0530

    Set up a custom editor for the MapGenerator

---
+++ 3D Shooter/Assets/Editor/MapEditor.cs
@@ -0,0 +1,22 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;																// namespace needed for editor scripts
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		MapGenerator map = target as MapGenerator;
+		
+		map.GenerateMap();
+	}
+}
--- 3D Shooter/Assets/Scripts/MapGenerator.cs
+++ 3D Shooter/Assets/Scripts/MapGenerator.cs
@@ -22,6 +22,17 @@ public class MapGenerator : MonoBehaviour
 
 	public void GenerateMap()
 	{
+		// the previous tiles need to be deleted before generating new ones, hence 
+		string holderName = "Generator Map";							// the string which will store the name of the gameobject
+		if(transform.Find(holderName))
+		{
+			// using DestroyImmediate() because it needs to be called from the editor
+			DestroyImmediate(transform.Find(holderName).gameObject);	// destroy the gameobject associated with the holdername string
+		}
+
+		Transform mapHolder = new GameObject(holderName).transform;
+		mapHolder.parent = transform;									//parenting the new mapholder to the parent script
+
 		for(int x = 0; x < mapSize.x; x++)
 		{
 			for(int y = 0; y < mapSize.y; y++)
@@ -32,6 +43,7 @@ public class MapGenerator : MonoBehaviour
 				Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right*90)) as Transform;
 
 				newTile.localScale = Vector3.one * (1 - outlinePercent);
+				newTile.parent = mapHolder;								//parenting the new tile to the mapholder, hierarchy matters, I guess...
 			}
 		}
 	}
