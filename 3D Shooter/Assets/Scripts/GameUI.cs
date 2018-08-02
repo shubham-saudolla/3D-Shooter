a5a1a4ad914017e47e3d4c585192bb16b78f0476
Shubham Saudolla <shubhamsaudolla@gmail.com>
2018-08-03 02:06:08 +0530

    Added the game UI

--- 3D Shooter/Assets/Scripts/GameUI.cs
+++ 3D Shooter/Assets/Scripts/GameUI.cs
@@ -14,10 +14,58 @@ public class GameUI : MonoBehaviour
     public Image fadePlane;
     public float fadeTime = 1f;
     public GameObject gameOverUI;
+    public RectTransform newWaveBanner;
+    public Text newWaveTitle;
+    public Text newWaveEnemyCount;
+    public float bannerSpeed = 2f;
+
+    private Spawner spawner;
 
     void Start()
     {
         FindObjectOfType<Player>().OnDeath += OnGameOver;
+    }
+
+    void Awake()
+    {
+        spawner = FindObjectOfType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
+
+    void OnNewWave(int waveNumber)
+    {
+        string[] numbers = { "One", "Two", "Three", "Four", "Five" };
+        newWaveTitle.text = "Wave " + numbers[waveNumber - 1];
+        string enemyCountString = ((spawner.waves[waveNumber - 1].infinite) ? "Infifinte" : spawner.waves[waveNumber - 1].enemyCount + "");
+        newWaveEnemyCount.text = "Enemies: " + enemyCountString;
+
+        StartCoroutine(AnimateNewWaveBanner());
+    }
+
+    IEnumerator AnimateNewWaveBanner()
+    {
+        float animatePercent = 0;
+        float delayTime = 1f;
+        float endDelayTime = Time.time + 1 / bannerSpeed + delayTime;
+        int dir = 1;
+
+        while (animatePercent >= 0)
+        {
+            animatePercent += Time.deltaTime * bannerSpeed * dir;
+
+            if (animatePercent >= 1)
+            {
+                animatePercent = 1;
+
+                if (Time.time > endDelayTime)
+                {
+                    dir = -1;
+                }
+            }
+
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-242, -26, animatePercent);
+            yield return null;
+        }
     }
 
     void OnGameOver()
