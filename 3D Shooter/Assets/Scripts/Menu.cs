401a66ccb2fadf8feac06e5ad13afbacd4997b09
Shubham Saudolla <shubhamsaudolla@gmail.com>
2018-08-04 20:31:20 +0530

    Added menu UI

---
+++ 3D Shooter/Assets/Scripts/Menu.cs
@@ -0,0 +1,62 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenuHolder;
+    public GameObject optionsMenuHolder;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenuHolder.SetActive(false);
+        optionsMenuHolder.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        optionsMenuHolder.SetActive(false);
+    }
+
+    public void SetScreenResolution(int i)
+    {
+
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+
+    }
+
+    public void SetMasterVolume(float value)
+    {
+
+    }
+
+    public void SetSfxVolume(float value)
+    {
+
+    }
+
+    public void SetMusicVolume(float value)
+    {
+
+    }
+}
