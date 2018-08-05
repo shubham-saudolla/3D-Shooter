20ef96932f95a302c54c242875fb4384f9fca199
Shubham Saudolla <shubhamsaudolla@gmail.com>
2018-08-05 10:41:54 +0530

    Improved the music manager

--- 3D Shooter/Assets/Scripts/MusicManager.cs
+++ 3D Shooter/Assets/Scripts/MusicManager.cs
@@ -6,22 +6,47 @@ https://github.com/shubham-saudolla
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
     public AudioClip mainTheme;
     public AudioClip menuTheme;
 
+    private string sceneName;
+
     void Start()
     {
-        AudioManager.instance.PlayMusic(menuTheme, 2);
+        OnLevelWasLoaded(0);
     }
 
-    void Update()
+    void OnLevelWasLoaded(int sceneIndex)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        string newSceneName = SceneManager.GetActiveScene().name;
+        if (newSceneName != sceneName)
         {
-            AudioManager.instance.PlayMusic(mainTheme, 3);
+            sceneName = newSceneName;
+            Invoke("PlayMusic", 0.2f);
+        }
+    }
+
+    void PlayMusic()
+    {
+        AudioClip clipToPlay = null;
+
+        if (sceneName == "Menu")
+        {
+            clipToPlay = menuTheme;
+        }
+        else if (sceneName == "Main")
+        {
+            clipToPlay = mainTheme;
+        }
+
+        if (clipToPlay != null)
+        {
+            AudioManager.instance.PlayMusic(clipToPlay, 2);
+            Invoke("PlayMusic", clipToPlay.length);
         }
     }
 }
